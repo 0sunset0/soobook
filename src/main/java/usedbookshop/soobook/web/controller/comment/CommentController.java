0834fe884e7cad06e6e32628334ad21f948c34cd
0sunset0 <chi6465618@naver.com>
2023-01-17 00:00:10 +0900

    Feat: "댓글 추가 함수"
    댓글 컨트롤러에 댓글 작성 함수 추가

--- src/main/java/usedbookshop/soobook/web/controller/comment/CommentController.java
+++ src/main/java/usedbookshop/soobook/web/controller/comment/CommentController.java
@@ -1,4 +1,25 @@
 package usedbookshop.soobook.web.controller.comment;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import usedbookshop.soobook.domain.Book;
+import usedbookshop.soobook.domain.Member;
+import usedbookshop.soobook.domain.Review;
+import usedbookshop.soobook.web.dto.review.AddReviewDto;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequiredArgsConstructor
 public class CommentController {
+
+    @PostMapping("review/addComment")
+    public String addComment(@ModelAttribute("addReviewDto") AddReviewDto addReviewDto, @RequestParam("reviewId") Long reviewId){
+
+        return "redirect:/book/review/detail?reviewId=" + reviewId;
+    }
 }
