1e2cb228cc8ca1427e7010e6a6a9e6062c9a54e7
0sunset0 <chi6465618@naver.com>
2023-02-06 18:45:13 +0900

    불필요한 코드 삭제

--- src/main/java/usedbookshop/soobook/exception/NotEnoughStockException.java
+++ src/main/java/usedbookshop/soobook/exception/NotEnoughStockException.java
@@ -2,20 +2,10 @@ package usedbookshop.soobook.exception;
 
 public class NotEnoughStockException extends RuntimeException {
 
-    public NotEnoughStockException() {
-        super();
-    }
 
     public NotEnoughStockException(String message) {
         super(message);
     }
 
-    public NotEnoughStockException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public NotEnoughStockException(Throwable cause) {
-        super(cause);
-    }
 
 }
--- src/main/java/usedbookshop/soobook/web/dto/member/LoginDto.java
+++ src/main/java/usedbookshop/soobook/web/dto/member/LoginDto.java
@@ -1,10 +1,7 @@
 package usedbookshop.soobook.web.dto.member;
 
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
-import usedbookshop.soobook.domain.Address;
-import usedbookshop.soobook.domain.Member;
 
 @Getter @Setter
 public class LoginDto {
