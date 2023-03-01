26ee22f3643d72309b52bc15c9db9726a78b1fe2
0sunset0 <chi6465618@naver.com>
2023-03-01 20:48:23 +0900

    Refactor: 로그인 여부 기능 인터셉터로 변경

--- src/main/java/usedbookshop/soobook/global/config/WebConfig.java
+++ src/main/java/usedbookshop/soobook/global/config/WebConfig.java
@@ -1,4 +1,29 @@
 package usedbookshop.soobook.global.config;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import usedbookshop.soobook.global.common.argumentresolver.LoginMemberArgumentResolver;
+import usedbookshop.soobook.global.common.interceptor.LoginCheckInterceptor;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginMemberArgumentResolver());
+    }
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/member/join", "/member/login", "/books", "/book/detail", "/book/review/detail",
+                        "/css/**", "/*.ico", "/error");
+    }
 }
